44a4f9845bc59b83cdc3aa3b77d4e937e2219ef1
Aslam Kureshi <kureshiaslam176@gmail.com>
2025-11-11 13:58:49 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const express = require('express');
 
 const app = express();
 
+const githubData = {
+  "login": "aslam8801",
+  "id": 235190780,
+  "node_id": "U_kgDODgS5_A",
+  "avatar_url": "https://avatars.githubusercontent.com/u/235190780?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/aslam8801",
+  "html_url": "https://github.com/aslam8801",
+  "followers_url": "https://api.github.com/users/aslam8801/followers",
+  "following_url": "https://api.github.com/users/aslam8801/following{/other_user}",
+  "gists_url": "https://api.github.com/users/aslam8801/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/aslam8801/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/aslam8801/subscriptions",
+  "organizations_url": "https://api.github.com/users/aslam8801/orgs",
+  "repos_url": "https://api.github.com/users/aslam8801/repos",
+  "events_url": "https://api.github.com/users/aslam8801/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/aslam8801/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 6,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2025-09-29T21:16:25Z",
+  "updated_at": "2025-09-29T21:16:25Z"
+}
+
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Welcome Back, Champ");
@@ -11,6 +47,10 @@ app.get("/", (req, res) => {
 
 app.get("/twitter", (req, res)=>{
     res.send('<a href="https://github.com/aslam8801">GitHub</a>')
+})
+
+app.get("/github", (req, res)=>{
+    res.json(githubData);
 })
 
 app.listen(PORT, ()=>{
